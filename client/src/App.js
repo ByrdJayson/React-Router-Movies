6968e32fd03fbaa251c769d43ff230f48dbe0a38
Jayson Byrd <jayson_byrd_95@outlook.com>
2021-11-08 16:00:37 -0500

    Home Button Directs to /

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, Route, Switch} from 'react-router-dom';
 import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 import SavedList from './Movies/SavedList';
 
@@ -33,7 +34,9 @@ export default function App () {
       <SavedList list={[ /* This is stretch */]} />
 
       <Switch>
-
+        <Route path='/movies/:id'>
+          <Movie />
+        </Route>
         <Route path='/'>
           <MovieList movies={movieList}/>
         </Route>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useParams } from 'react';
 import axios from 'axios';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { id } = useParams();
+  console.log(id)
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
   useEffect(() => {
